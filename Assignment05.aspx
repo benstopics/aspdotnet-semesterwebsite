292f7d1b9bfec641fbe826c6b297b98330b43754
benjamin d ward <benstopics@gmail.com>
2016-03-16 15:12:15 -0400

    save assignment 05

---
+++ Assignment05.aspx
@@ -0,0 +1,26 @@
+﻿<%--
+Assignment05: Markup
+Benjamin Ward
+2/17/2016
+wardb3@mail.uc.edu
+--%>
+
+<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Assignment05.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <p>
+        <asp:Label ID="lblModel" runat="server" Text="Choose your model: "></asp:Label>
+        <asp:DropDownList ID="ddModel" runat="server" Height="16px" Width="195px" DataSourceID="sdsModel" DataTextField="Model" DataValueField="ModelID" OnSelectedIndexChanged="ddModel_SelectedIndexChanged" >
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="sdsModel" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [tModel]"></asp:SqlDataSource>
+    </p>
+    <asp:ListBox ID="lbLeftSide" runat="server" Width="250px" DataSourceID="sdsLeftListBox" DataTextField="VehicleOption" DataValueField="VehicleOptionID"></asp:ListBox>
+    <asp:SqlDataSource ID="sdsLeftListBox" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [vModelAndOption]"></asp:SqlDataSource>
+    <asp:Button ID="btnAllRight" runat="server" Text=">>" OnClick="btnAllRight_Click" />
+    <asp:Button ID="btnRight" runat="server" Text=">" OnClick="btnRight_Click" />
+    <asp:Button ID="btnLeft" runat="server" Text="<" OnClick="btnLeft_Click" />
+    <asp:Button ID="btnAllLeft" runat="server" Text="<<" OnClick="btnAllLeft_Click" />
+    <asp:ListBox ID="lbRightSide" runat="server" Width="250px"></asp:ListBox>
+</asp:Content>
---
+++ Assignment05.aspx.cs
@@ -0,0 +1,91 @@
+﻿/**************************************************************
+ * Connect to DB and provide UI for designing car model       *
+ * Bill Nicholson                                             *
+ * nicholdw@ucmail.uc.edu                                     *
+ * ************************************************************
+ * Assignment05: Code-behind                                  *
+ * Benjamin Ward                                              *
+ * 2/17/2016                                                  *
+ * wardb3@mail.uc.edu                                         *
+ *************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+    protected void Page_LoadComplete(object sender, EventArgs e)
+    {
+        ddModel.AutoPostBack = true;
+        if (!IsPostBack)
+            UpdateOptions();
+    }
+    private void UpdateOptions()
+    {
+        sdsLeftListBox.SelectParameters.Clear();
+        sdsLeftListBox.SelectParameters.Add("id", ddModel.SelectedValue);
+        sdsLeftListBox.SelectCommand = "SELECT * FROM vModelAndOption WHERE ModelID = @id";
+        lbRightSide.Items.Clear();
+        lbLeftSide.ClearSelection();
+        lbRightSide.ClearSelection();
+    }
+    protected void ddModel_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        UpdateOptions();
+    }
+    protected void btnAllRight_Click(object sender, EventArgs e)
+    {
+        List<string> leftItems = new List<string>();
+
+        foreach (ListItem item in lbLeftSide.Items)
+        {
+            lbRightSide.Items.Add(item);
+        }
+
+        lbLeftSide.Items.Clear();
+        lbLeftSide.ClearSelection();
+        lbRightSide.ClearSelection();
+    }
+    protected void btnAllLeft_Click(object sender, EventArgs e)
+    {
+        List<string> rightItems = new List<string>();
+
+        foreach (ListItem item in lbRightSide.Items)
+        {
+            lbLeftSide.Items.Add(item);
+        }
+
+        lbRightSide.Items.Clear();
+        lbLeftSide.ClearSelection();
+        lbRightSide.ClearSelection();
+    }
+    protected void btnRight_Click(object sender, EventArgs e)
+    {
+        if (lbLeftSide.SelectedItem != null)
+        {
+            lbRightSide.Items.Add(lbLeftSide.SelectedItem);
+            lbLeftSide.Items.Remove(lbLeftSide.SelectedItem);
+        }
+
+        lbLeftSide.ClearSelection();
+    }
+    protected void btnLeft_Click(object sender, EventArgs e)
+    {
+        if (lbRightSide.SelectedItem != null)
+        {
+            lbLeftSide.Items.Add(lbRightSide.SelectedItem);
+            lbRightSide.Items.Remove(lbRightSide.SelectedItem);
+        }
+
+        lbRightSide.ClearSelection();
+    }
+}
